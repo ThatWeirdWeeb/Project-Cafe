d1be3d8b608d331ee4be62d8b0eaf3d27f4d3356
johnp <johnp@DESKTOP-JMEP13>
2020-03-21 20:17:54 +0800

    Initial commit

--- src/com/jmachieved/projectcafe/contents/runner/runnerSS.java
+++ src/com/jmachieved/projectcafe/contents/runner/runnerSS.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jmachieved.projectcafe.contents.helpers;
+package com.jmachieved.projectcafe.contents.runner;
 
 import com.jmachieved.projectcafe.contents.Home;
 import com.jmachieved.projectcafe.contents.splashscreen;
